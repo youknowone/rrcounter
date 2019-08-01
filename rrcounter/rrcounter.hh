90f14bbf23a2b18db3d368a7a97076ff683c74d4
Jeong Yunwon <jeong@youknowone.org>
2019-08-01 16:44:32 +0900

    Fix warnings

--- rrcounter/rrcounter.hh
+++ rrcounter/rrcounter.hh
@@ -38,7 +38,7 @@ class RoundRobinCounter {
         }
       }
       this->summation = 0;
-      for (int i = 0; i < _size; i++) {
+      for (size_t i = 0; i < _size; i++) {
         this->summation += this->slots[i];
       }
       this->last_index = current_index;
